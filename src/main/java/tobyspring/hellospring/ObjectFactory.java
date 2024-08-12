6f0665a5ec8e8dc50374003812da3628f85e94fb
babyfox225 <babyfox225@naver.com>
2024-08-12 21:02:25 +0900

    refactor: @ComponentScan, @Component 적용

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,11 +1,12 @@
 package tobyspring.hellospring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan // 해당 패키지 동/하위 클래스 스캔
 public class ObjectFactory {
-    @Bean
+/*    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
@@ -13,5 +14,5 @@ public class ObjectFactory {
     @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
-    }
+    }*/
 }
