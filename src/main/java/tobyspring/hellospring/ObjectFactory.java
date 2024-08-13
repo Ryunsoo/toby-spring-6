89dc84cef5650bd49863beef76403eb7b2371460
babyfox225 <babyfox225@naver.com>
2024-08-13 21:56:04 +0900

    feat: CachedExRateProvider 기능 추가
    - Decorator pattern 적용

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,18 +1,22 @@
 package tobyspring.hellospring;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan // 해당 패키지 동/하위 클래스 스캔
 public class ObjectFactory {
-/*    @Bean
+    @Bean
     public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
+        return new PaymentService(cachedExRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
     }
 
     @Bean
     public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }*/
+        return new WebApiExRateProvider();
+    }
 }
